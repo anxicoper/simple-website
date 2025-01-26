c6f367afaff822888249faac38694b6b2484fddf
anxicoper <angelicacorzop123@gmail.com>
2025-01-26 12:46:13 -0500

    Add js and css

---
+++ app.js
@@ -0,0 +1,18 @@
+app.js
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log('current Class name: ' + className);
+});
